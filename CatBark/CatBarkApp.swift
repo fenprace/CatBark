5b0b4265d4be499cd860eedb9f6b78b939973e72
Zhuo Feng <zhuo.feng@shopee.com>
2022-06-16 17:35:48 +0800

    Initial Commit

---
+++ CatBark/CatBarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatBarkApp.swift
+//  CatBark
+//
+//  Created by Zhuo FENG on 2022/6/16.
+//
+
+import SwiftUI
+
+@main
+struct CatBarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
